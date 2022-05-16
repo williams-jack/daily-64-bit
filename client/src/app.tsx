13101aa765eb0d810d19b40b6b3bed1f1d5770da
williams-jack <williams.jack@northeastern.edu>
2022-05-16 12:30:04 -0400

    Removed extraneous files. Implemented default dark mode.

--- client/src/app.tsx
+++ client/src/app.tsx
@@ -1,10 +1,11 @@
+import { Container } from '@chakra-ui/react';
 import React from 'react';
 
 function App() {
   return (
-    <div className="App">
-      Hello World!    
-    </div>
+    <Container>
+      Hello
+    </Container>   
   );
 }
 
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,14 +1,19 @@
+import { ChakraProvider, ColorModeScript } from '@chakra-ui/react';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './app';
+import theme from './styles/theme';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <>
+    <ChakraProvider theme={theme}>
+      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+      <App />
+    </ChakraProvider>
+  </>
 );
 
 // If you want to start measuring performance in your app, pass a function
